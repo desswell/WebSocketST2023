6bf75a40b54ffd217429453d2176e7d23ad87b48
desswell <46073080+desswell@users.noreply.github.com>
2023-05-24 04:04:16 +0300

    done

--- webSocketSt2023/websocket.py
+++ webSocketSt2023/websocket.py
@@ -1,5 +1,7 @@
 import asyncio
 import json
+import time
+
 import grpc
 import websockets
 import not_pb2
@@ -7,30 +9,31 @@ import not_pb2_grpc
 connected = set()
 disconnected = set()
 message_cache = []
-connected_user_id = {}
-
+connected_user_login = {}
+global_message = [0]
 async def websocket_handler(websocket):
     print('ws handler')
     connected.add(websocket)
     try:
         while True:
-            message = await websocket.recv()
-            message = json.loads(message)
-            print(connected_user_id)
-            if len(message.keys()) == 1:
-                connected_user_id[websocket] = message["id"]
-                print(connected_user_id)
-            else:
-                print(f"Received message: {message}")
-                with grpc.insecure_channel('localhost:7000') as channel:
+            with grpc.insecure_channel('localhost:7010') as channel:
+                message = await websocket.recv()
+                message = json.loads(message)
+                print(connected_user_login)
+                if len(message.keys()) == 1:
+                    connected_user_login[websocket] = message["login"]
+                    print(connected_user_login)
+                else:
+                    print(f"Received message: {message}")
+                    global_message[0] = message["id"]
                     stub = not_pb2_grpc.send_notificationsStub(channel=channel)
                     response = stub.sendNotifications(not_pb2.notifications(data=[message]))
     except websockets.exceptions.ConnectionClosed:
         print("WebSocket connection closed")
         disconnected.add(websocket)
     finally:
-        if connected_user_id:
-            del connected_user_id[websocket]
+        if connected_user_login:
+            del connected_user_login[websocket]
         connected.remove(websocket)
 
 
@@ -61,11 +64,9 @@ async def handle_grpc(reader, writer):
     print('handle grpc')
 
     data = await reader.read(1024)
-    print(data)
     message_cache.append(data)
     if len(message_cache) == 31:
         del message_cache[0]
-
     await send_data_via_websocket(data=data)
     writer.close()
 
@@ -80,8 +81,18 @@ async def start_grpc_server():
 
 
 async def send_data_via_websocket(data):
-    for connection in connected:
-        await connection.send(str(data)[2: -1])
+    data = data.decode('utf-8')
+    data = data[1:]
+    data = data[:-1]
+    data = json.loads(data)
+    username = data['username']
+    reverseConnected_user_login = dict(zip(connected_user_login.values(), connected_user_login.keys()))
+    connection = reverseConnected_user_login.get(username)
+    data_to_send = str(global_message[0])
+    print(data_to_send)
+    if data_to_send:
+        await connection.send(data_to_send)
+        time.sleep(1)
 
 
 async def run_websocket_server():
